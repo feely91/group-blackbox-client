032452616b5e9a61c0d39c7ed9e255781329577d
Adam Feely <a.feely@kainos.com>
2015-09-04 11:56:51 +0100

    Fix empty FriendClient class

--- src/com/kainos/training/blackbox/client/FriendClient.java
+++ src/com/kainos/training/blackbox/client/FriendClient.java
@@ -1,4 +1,47 @@
 package com.kainos.training.blackbox.client;
 
-public class BaseClient {
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.kainos.training.blackboxinterface.model.person.Person;
+
+public class FriendClient {
+
+	// "http://localhost:8910/person"
+	private Client client;
+	private String serverRoot;
+	private WebTarget webTarget;
+
+	public FriendClient(String serverAddress) {
+		client = ClientBuilder.newClient();
+		serverRoot = serverAddress;
+
+		webTarget = client.target(serverRoot);
+	}
+
+	public Response addFriend(Person person) {
+		Response r = webTarget.request().post(
+				Entity.entity(person, MediaType.APPLICATION_JSON));
+		return r;
+	}
+
+	public Response removeFriend(Person person) {
+
+		WebTarget deleteTarget = webTarget.path("/" + person.getId());
+		Response r = deleteTarget.request().delete();
+		return r;
+	}
+
+	public List<Person> getFriendsList() {
+		Response response = webTarget.request().get();
+		return response.readEntity(new GenericType<List<Person>>() {
+		});
+	}
 }
